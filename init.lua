81fde36aa33e89d4d9bffcfd4babf691a642e896
russoul <me@russoul.me>
2024-06-20 21:22:27 +0400

    Include some changes from the corporate laptop

--- init.lua
+++ init.lua
@@ -131,6 +131,10 @@ vim.cmd[[Plug 'simrat39/rust-tools.nvim']]
 vim.cmd[[Plug 'scalameta/nvim-metals']]
 -- Plugin that enables fugitive's GBrowse open links to private repos?
 vim.cmd[[Plug 'https://github.com/tpope/vim-rhubarb']]
+-- A buffer for showing diagnostics, references, telescope results, quickfix and location lists
+vim.cmd[[Plug 'folke/trouble.nvim', {'commit' : '3f85d8ed30e97ceeddbbcf80224245d347053711'}]]
+-- Show git blame
+vim.cmd[[Plug 'FabijanZulj/blame.nvim', {'commit' : '3e6b2ef4905982cd7d26eb5b18b0e761138eb5ab'}]]
 vim.cmd[[call plug#end()]]
 
 -- Highlight on yank (vanity)
@@ -190,7 +194,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "vim,idris2,python,javascript,lua,c,hott,nova,txt,agda",
+    pattern = "vim,idris2,python,javascript,lua,c,hott,nova,txt,agda,scala",
     callback = function(args)
       vim.o.number = true
     end,
@@ -428,7 +432,17 @@ rt.setup({
   },
 })
 
-require'lspconfig'.metals.setup{}
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt", "java" },
+  callback = function()
+    require("metals").initialize_or_attach({})
+  end,
+  group = nvim_metals_group,
+})
+metals_config = require("metals").bare_config()
+metals_config.init_options.statusBarProvider = "on"
+
 require'lspconfig'.clangd.setup{}
 
 require('bufferline').setup {
@@ -728,3 +742,59 @@ vim.api.nvim_set_hl(0, 'LeapLabelSecondary', {
 -- require('leap').opts.highlight_unlabeled_phase_one_targets = true
 
 -----------------------------
+
+require("trouble").setup{
+    position = "bottom", -- position of the list can be: bottom, top, left, right
+    height = 10, -- height of the trouble list when position is top or bottom
+    width = 50, -- width of the list when position is left or right
+    icons = true, -- use devicons for filenames
+    mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+    severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+    fold_open = "", -- icon used for open folds
+    fold_closed = "", -- icon used for closed folds
+    group = true, -- group results by file
+    padding = true, -- add an extra new line on top of the list
+    cycle_results = true, -- cycle item list when reaching beginning or end of list
+    action_keys = { -- key mappings for actions in the trouble list
+        -- map to {} to remove a mapping, for example:
+        -- close = {},
+        close = "q", -- close the list
+        cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+        refresh = "r", -- manually refresh
+        jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
+        open_split = { "<c-x>" }, -- open buffer in new split
+        open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+        open_tab = { "<c-t>" }, -- open buffer in new tab
+        jump_close = {"o"}, -- jump to the diagnostic and close the list
+        toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+        switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
+        toggle_preview = "P", -- toggle auto_preview
+        hover = "K", -- opens a small popup with the full multiline message
+        preview = "p", -- preview the diagnostic location
+        open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
+        close_folds = {"zM", "zm"}, -- close all folds
+        open_folds = {"zR", "zr"}, -- open all folds
+        toggle_fold = {"zA", "za"}, -- toggle fold of current file
+        previous = "i", -- previous item
+        next = "k", -- next item
+        help = "?" -- help menu
+    },
+    multiline = true, -- render multi-line messages
+    indent_lines = true, -- add an indent guide below the fold icons
+    win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
+    auto_open = false, -- automatically open the list when you have diagnostics
+    auto_close = false, -- automatically close the list when you have no diagnostics
+    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+    auto_fold = false, -- automatically fold a file trouble list at creation
+    auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+    include_declaration = { "lsp_references", "lsp_implementations", "lsp_definitions"  }, -- for the given modes, include the declaration of the current symbol in the results
+    signs = {
+      -- icons / text used for a diagnostic
+      error = "",
+      warning = "",
+      hint = "",
+      information = "",
+      other = "",
+    },
+    use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+}
